5c87fe40646e8a827b5a42fb931357cf644c86c2
Ca <atsabanoz@gmail.com>
2023-11-01 11:11:47 +0300

    Project

---
+++ MyProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+using System.Diagnostics;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class AccountData
+        {
+            public string NickName { get; set; }
+            public string Password { get; set; }
+
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult blog()
+        {
+
+            return View();
+        }
+
+        public IActionResult blog2()
+        {
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult account(AccountData model)
+        {
+
+            return View("Index");
+        }
+    }
+}
